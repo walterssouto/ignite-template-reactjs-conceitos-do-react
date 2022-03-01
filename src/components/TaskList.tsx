6a12b66c30cef2c2c14bda4c78f4d829a4cf0f89
Walters Souto <56978845+walterssouto@users.noreply.github.com>
2022-03-01 09:39:13 -0300

    tarefa completa

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,15 +16,29 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+      const newTask = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false
+      }
+    
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const filteredTask = tasks.filter(task => task.id != id);
+    setTasks(filteredTask);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTask = tasks.filter(task => task.id != id);
+    setTasks(filteredTask);
   }
+  
 
   return (
     <section className="task-list container">
